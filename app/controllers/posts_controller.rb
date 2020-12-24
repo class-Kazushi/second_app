cb9f1ddc4c30ed1b5fa9982461b419c9ebf86dda
class-Kazushi <k.a.z928marine@gmail.com>
2020-12-24 09:46:22 +0900

    複数のデータを表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index  # indexアクションの定義
-    @post = Post.find(1)  # 1番目のレコードを@postに代入
+    @posts = Post.all  # すべてのレコードを@postsに代入
   end
 
 end
