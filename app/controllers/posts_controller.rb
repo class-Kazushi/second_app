659a61e5bbf4b1d9ee9f269e13018b2a8619f5c5
class-Kazushi <k.a.z928marine@gmail.com>
2021-01-17 22:26:16 +0900

    newアクションの削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,6 @@ class PostsController < ApplicationController
     @posts = Post.all.order(id: "DESC")  # すべてのレコードを@postsに代入
   end
 
-  def new
-  end
-
   def create
     Post.create(content: params[:content])
   end
